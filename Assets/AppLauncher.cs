a2a68a1d085e859254d15e527fcfd4a006abfab4
shole <pahappo@gmail.com>
2017-12-02 16:22:42 +0200

    basic back&forth functional

--- Assets/AppLauncher.cs
+++ Assets/AppLauncher.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections;
 using System.IO;
 using System.Diagnostics;
+using System.Text;
+using System.Threading;
 using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
@@ -22,45 +24,45 @@ public class AppLauncher : MonoBehaviour {
 	// https://stackoverflow.com/questions/611094/async-process-start-and-wait-for-it-to-finish
 	
 	IEnumerator sendPoll() {
-		Debug.Log("a");
+		//Debug.Log("a");
 		while ( true ) {
-			Debug.Log("b");
-			yield return null;
-			yield return null;
-			yield return null;
-			yield return null;
-			yield return null;
-			yield return null;
-			yield return null;
-			yield return null;
-			yield return null;
-			Debug.Log("c");
+			//Debug.Log("b");
+			yield return new WaitForSeconds(1);
+			
 			float outval = Random.Range(1f, 100f);
 			Debug.Log("stdin " + outval);
-			messageStream.WriteLine("" + outval);
-			//messageStream.Flush();
+			process.StandardInput.WriteLine("" + outval);
+			process.StandardInput.Flush();
+			
 		}
 	}
 
 	void StartProcess() {
 		try {
 			process = new Process();
-			process.EnableRaisingEvents = false;
 			process.StartInfo.FileName = execPath;
 			process.StartInfo.Arguments = execArgs;
+			
 			process.StartInfo.UseShellExecute = false;
 			process.StartInfo.RedirectStandardOutput = true;
 			process.StartInfo.RedirectStandardInput = true;
 			process.StartInfo.RedirectStandardError = true;
-			//process.StartInfo.CreateNoWindow = true;
+			//process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+			//process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
+			process.StartInfo.CreateNoWindow = true;
+			process.EnableRaisingEvents = true;
+			
 			process.OutputDataReceived += DataReceived;
 			process.ErrorDataReceived += ErrorReceived;
+			
 			process.Start();
+			
 			process.BeginOutputReadLine();
-			messageStream = process.StandardInput;
-			messageStream.AutoFlush = true;
+			process.BeginErrorReadLine();
+			process.StandardInput.AutoFlush = true;
 
 			Debug.Log("Successfully launched app");
+			//process.WaitForExit();
 		} catch ( Exception e ) {
 			Debug.LogError("Unable to launch app: " + e.Message);
 		}
@@ -68,6 +70,7 @@ public class AppLauncher : MonoBehaviour {
 
 	void DataReceived(object sender, DataReceivedEventArgs eventArgs) {
 		Debug.Log("stdout " + eventArgs.Data);
+		//Debug.Log("stdout " + process.StandardOutput.ReadToEnd());
 	}
 
 	void ErrorReceived(object sender, DataReceivedEventArgs eventArgs) {
--- answertest.py
+++ answertest.py
@@ -1,9 +1,32 @@
-
 import sys
+import time
 
-print("afsdfds");
+#while True:
+#time.sleep(1)
+print("OK, START")
 
-for line in sys.stdin:
-    if (line=="QUIT"):
-        exit()
-    print(float(line)*2)
+sys.stdout.flush()
+
+while True:
+    line = sys.stdin.readline().strip()
+
+    f=file("stdin.txt",mode='a')
+
+    if len(line) > 0:
+        if line.upper() == "QUIT":
+            print("OK, QUIT")
+            exit(0)
+        try:
+            print("OK, "+str(float(line)*2))
+            #sys.stdout.write("OK, "+str(float(line)*2)+"\n")
+            f.write("ok ")
+        except:
+            print("Parse error, "+line)
+            f.write("error ")
+        sys.stderr.write('spam\n')
+
+    sys.stdout.flush()
+    f.write(line+'\n')
+    f.close()
+
+
